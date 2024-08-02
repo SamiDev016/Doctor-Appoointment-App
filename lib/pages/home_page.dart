ab2414fa8d95e5bd1a710d21e811b552bfd8324c
samidev016 <adelim0555@gmail.com>
2024-08-02 16:46:14 +0200

    Starting home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:appointment_application/utils/config.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,8 +11,48 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Home page"),),
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 15
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Adel",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/profile_image.jpg'),
+                    ),
+                  )
+                ],
+              ),
+              Config.spaceSmall,
+
+              Text(
+                'Category',
+                style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
