3dab957e8526d13d517777815343ed343cc0a304
samidev016 <adelim0555@gmail.com>
2024-08-02 17:06:41 +0200

    category

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:appointment_application/utils/config.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,28 +10,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> medCat = [
+    {"icon": FontAwesomeIcons.userDoctor, "Category": "General"},
+    {"icon": FontAwesomeIcons.heartPulse, "Category": "Cardiology"},
+    {"icon": FontAwesomeIcons.lungs, "Category": "Respirations"},
+    {"icon": FontAwesomeIcons.hand, "Category": "Dermatology"},
+    {"icon": FontAwesomeIcons.personPregnant, "Category": "Gynecology"},
+    {"icon": FontAwesomeIcons.teeth, "Category": "Dental"},
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 15
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
         child: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
                     "Adel",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold
-                    ),
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(
                     child: CircleAvatar(
@@ -40,15 +43,54 @@ class _HomePageState extends State<HomePage> {
                   )
                 ],
               ),
+              Config.spaceMedium,
+              const Text(
+                'Category',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Config.spaceSmall,
+              SizedBox(
+                height: Config.heightSize * 0.05,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: List<Widget>.generate(medCat.length, (index) {
+                    return Card(
+                      margin: const EdgeInsets.only(right: 20),
+                      color: Config.primaryColor,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            FaIcon(
+                              medCat[index]["icon"],
+                              color: Colors.white,
+                            ),
+                            Config.spaceSmall,
+                            Text(
+                              medCat[index]["Category"],
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+              Config.spaceSmall,
+              const Text(
+                "Appointment",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
               Config.spaceSmall,
 
-              Text(
-                'Category',
-                style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold
-                    ),
-              )
+              
             ],
           ),
         ),
